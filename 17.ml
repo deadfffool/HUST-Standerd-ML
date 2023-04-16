71d808df25092d8abe76a09a585c56a7b6417f84
yaochenxuan <deadfffool@gmail.com>
2023-04-16 15:15:00 +0800

    miles

--- 17.ml
+++ 17.ml
@@ -46,26 +46,29 @@ fun getIntInfTable ( 0 ) = []
 (*****Begin*****)
 val num = getInt();
 val list = getIntTable(num);
-fun compare([], n) = (0, 0)
+fun compare([], n) = (0, n)
     | compare(x :: xs, n) =
     if(hd xs > x) then compare(xs, n + 1)
-    else (hd xs, n);
-fun findp([], n, x1) = 0
-    | findp(x :: xs, n, x1) = 
-    if(x > x1) then (x)
+    else (hd xs, n+1);
+
+fun findp([], n : int, x1 : int) = (0,n)
+    | findp(x :: xs, n : int , x1 : int ) = 
+    if(x > x1) then (x,n)
     else findp(xs, n + 1, x1);
-fun exchange([], x1 : int, x2 : int) = []
-    |exchange(x :: xs, x1 : int, x2 : int) =
-    if x = x1 then x2 :: exchange(xs, x1, x2)
-    else if x = x2 then x1 :: exchange(xs, x1, x2)
-    else x :: exchange(xs, x1, x2);
+    
 val v1 = compare(list, 0);
-val x1 = #1 v1;
-val x2 = findp(list, 0, x1);
-val p1 = exchange(list, x1, x2);
-val p2 = List.take(p1, #2 v1 + 1);
-val p3 = List.drop(p1, #2 v1 + 1);
-val p4 = List.rev(p2);
-val result = p4 @ p3;
-printIntTable(result);
+val x1 = #1 v1 ;
+val n1 = #2 v1;
+val v2 = findp(list, 0, x1);
+val x2 = #1 v2 ;
+val n2 = #2 v2;
+
+val p1 = List.take(list,n2);
+val p2 = List.take(List.drop(list,n2+1),n1-n2-1);
+val p3 = List.drop(list,n1+1);
+
+val p4 = List.rev(p1 @ (x1 :: []) @ p2);
+printIntTable(p4 @ (x2 :: []) @ p3);
+
+printIntTable(List.take(list,0));
 (*****End*****)
